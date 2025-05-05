e168a37b0ad64b4b297dd80e0144e793fbc409c1
Biokes <106444765+Biokes@users.noreply.github.com>
2025-05-05 10:25:28 +0100

    project initialization

--- src/components/commons/Navbar.tsx
+++ src/components/commons/Navbar.tsx
@@ -1,16 +1,11 @@
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoSearch } from "react-icons/io5";
-
+import '../../App.css'
 export default function Navbar() {
   return (
     <nav>
-      <header className='flex justify-between pt-2 w-full px-1 md:px-3'>
-        <img src="/logo.webp" alt="" className={'h-[38px] w-[158px]'} />
-        <section className={'flex gap-2 pt-2'}>
-          <IoSearch color={'white'} className='cursor-pointer w-[24px] h-[24px]'/>
-          <RxHamburgerMenu color={'white'} className='cursor-pointer w-[27px] h-[24px]'/>
-        </section>
-      </header>
-      </nav>
+      p
+    </nav>
   )
 }
+  
