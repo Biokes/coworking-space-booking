910dab31c8d8f68f1c52535d78da8de58db6d220
Biokes <106444765+Biokes@users.noreply.github.com>
2025-05-05 11:59:56 +0100

    update

--- src/components/commons/Navbar.tsx
+++ src/components/commons/Navbar.tsx
@@ -1,9 +1,8 @@
 import '../../App.css'
 export default function Navbar() {
   return (
-    <nav className='bg-gray-800 flex justify-between items-center px-3'>
+    <nav className='bg-gray-800 flex justify-start items-start px-3'>
       <h5 className='italic cursor-pointer'>Booker</h5>
-      <button href="#booking" className="px-[10px] text-gray-500">Book now</button>
     </nav>
   )
 }
